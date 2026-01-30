4ac31276a8f50c185422f8f2f3437dafe8386dda
DerIngo <21258298+DerIngo@users.noreply.github.com>
2026-01-30 23:21:14 +0100

    Expand filter and paste options

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
-export const FILTER_NAMES = ['blur'] as const
-export const PASTE_ELLIPSE_NAMES = ['feathered'] as const
+export const FILTER_NAMES = ['blur', 'pixelate', 'line_mosaic', 'facet_effect', 'verwischung_1'] as const
+export const PASTE_ELLIPSE_NAMES = ['feathered', 'hard'] as const
 
 export type FilterName = (typeof FILTER_NAMES)[number]
 export type PasteEllipseName = (typeof PASTE_ELLIPSE_NAMES)[number]
